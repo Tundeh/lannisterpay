2b86569f00acdbc982ccae541e8c794887c1b749
Tundeh <jamiu4freedom@gmail.com>
2022-07-17 02:03:29 +0100

    add enviromental variables

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require("http");
 const app = require("./app");
 
-app.set("port", 4000);
+app.set("port", process.env.PORT || 4000);
 
 const server = http.createServer(app);
 
-server.listen(4000);
+server.listen(process.env.PORT || 4000);
