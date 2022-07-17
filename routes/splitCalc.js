3d0556d81a6f5109ee706f7a62f037326fd8844f
Tundeh <jamiu4freedom@gmail.com>
2022-07-17 01:49:32 +0100

    create computation module

---
+++ routes/splitCalc.js
@@ -0,0 +1,69 @@
+const splitCalc = (data) => {
+
+    
+const {ID, Amount, SplitInfo} = data;
+const splitLength = SplitInfo.length;
+let Balance = Amount;
+let SplitBreakdown = [];
+let result = {
+    ID,
+    Balance
+};
+let totalRatio = 0;
+const splitRatio = [];
+const splitPerc = [];
+
+SplitInfo.forEach(element => {
+    let {SplitType, SplitValue, SplitEntityId} = element;
+    if(SplitType == 'FLAT') {
+        Balance = Balance - SplitValue;
+        SplitBreakdown.push({
+            SplitEntityId,
+            "Amount": SplitValue
+        })
+    } else if (SplitType == 'PERCENTAGE') {
+        splitPerc.push(element);
+    } else if(SplitType == 'RATIO') {
+        splitRatio.push(element)
+        totalRatio = totalRatio + element.SplitValue;
+    } 
+});
+
+if(splitPerc.length>0){
+    splitPerc.forEach(element => {
+        let {SplitValue, SplitEntityId} = element;
+        let perc = ((SplitValue * Balance) / 100);
+        Balance = Balance - perc;
+        SplitBreakdown.push({
+            SplitEntityId,
+            "Amount": perc,
+    
+        })
+    
+    }
+    
+    );
+}
+
+if(splitRatio.length>0){
+
+    splitRatio.forEach( element => {
+        let {SplitValue, SplitEntityId} = element;
+        let ratioValue = (SplitValue * Balance) / totalRatio;
+        SplitBreakdown.push({
+            SplitEntityId,
+            "Amount": ratioValue,
+    
+        })
+        
+    })
+    Balance = 0;
+}
+
+if(Balance<0){
+    Balance = 0;
+}
+return {...result, SplitBreakdown, Balance};
+}
+
+module.exports = splitCalc;
